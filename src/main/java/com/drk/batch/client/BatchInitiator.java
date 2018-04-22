6918393256ec0a20179c7b2421249b017f1b621a
Deepak Raj <deepak1696@gmail.com>
2018-04-22 10:36:33 -0700

    Updating the pom file and package name

--- src/main/java/com/drk/batch/client/BatchInitiator.java
+++ src/main/java/com/drk/batch/client/BatchInitiator.java
@@ -1,7 +1,8 @@
-package com.drk.batch.utils;
+package com.drk.batch.client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class BatchInitiator {
